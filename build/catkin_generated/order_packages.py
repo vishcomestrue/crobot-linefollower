92dc18c53c72b8804e55e0c080be69d90002c7a3
vishcomestrue <ramvishwa03@gmail.com>
2023-04-14 17:43:55 +0530

    Initial commit - added a custom bot by writing own urdf

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/rosmachine/project_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/rosmachine/project_ws/devel;/opt/ros/noetic'.split(';') if '/home/rosmachine/project_ws/devel;/opt/ros/noetic' != '' else []
---
+++ build/miniproj/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/rosmachine/project_ws/src/miniproj
+# Build directory: /home/rosmachine/project_ws/build/miniproj
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
